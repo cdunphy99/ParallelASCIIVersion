e834f2250c72f9b026364c4b20f764fe92326dff
BuildTools <unconfigured@null.spigotmc.org>
2020-04-09 16:15:30 -0400

    update to mike's changes

--- ParallelASCIIVersion/RowTree.h
+++ ParallelASCIIVersion/RowTree.h
@@ -1,9 +1,8 @@
 #pragma once
 #include <string>
-#include <vector>
 #include "NodeObject.h"
 
-#define NUMVALUES 3
+#define NUMVALUES 26
 #define startPoint 97
 
 using namespace std;
@@ -15,7 +14,7 @@ public:
 	RowTree(int);
 	RowTree(RowTree*);
 
-	vector<string> getPermutations();
+	string* getPermutations();
 	int getLen();
 
 	~RowTree() = default;
@@ -28,20 +27,16 @@ private:
 };
 
 RowTree::RowTree(int startingLetter) {
-	cout << "---" << len << "====\n"; // does not run this
-
-	this->level = 1;
+	this->level = 2;
 	this->len = NUMVALUES;
-	cout << "---" << len << "====\n";
 	RowList = new NodeObject[NUMVALUES];
 	for (int i = 0; i < (NUMVALUES); i++)
 	{
-		
+		NodeObject* temp = new NodeObject;
 		int* temp2 = new int[2];
 		temp2[0] = startingLetter;
 		temp2[1] = i + startPoint;
-		NodeObject* temp = new NodeObject(temp2, level);
-
+		temp->setWord(temp2);
 		RowList[i] = *temp;
 	}
 }
@@ -49,14 +44,12 @@ RowTree::RowTree(int startingLetter) {
 RowTree::RowTree(RowTree* prevRow) {
 	// length of previous * num of possible values
 	this->len = prevRow->getLen() * NUMVALUES;
-	cout << "RowTree prevRow constructor: updating len to " << prevRow->getLen() * NUMVALUES << "\n";
 	this->level = prevRow->level + 1;
 	this->RowList = new NodeObject[this->len];
-	cout << "FUCK\n"; // does not get here
 
 	for (int i = 0; i < prevRow->len; i++) {
-		cout << "FUCK\n"; // does not get here
 		for (int j = 0; j < NUMVALUES; j++) {
+
 			NodeObject* temp = new NodeObject;
 			int* temp2 = new int[level];
 			int* temp3 = prevRow->RowList[i].getIntWord();
@@ -65,20 +58,19 @@ RowTree::RowTree(RowTree* prevRow) {
 				temp2[k] = temp3[k];
 			}
 			temp2[level - 1] = startPoint + j;
-			temp->setWord(temp2, level);
+			temp->setWord(temp2);
 
 			this->RowList[(i * NUMVALUES) + j] = *temp;
 		}
 	}
 }
 
-inline vector<string> RowTree::getPermutations()
+inline string* RowTree::getPermutations()
 {
-	vector<string> Permutations;
+	string* Permutations = new string[this->len];
 	for (int i = 0; i < this->len; i++)
 	{
-		Permutations.push_back(RowList[i].getWord());
-		cout << "Added " << Permutations[i] << " to permutations list\n";
+		Permutations[i] = RowList[i].getWord();
 	}
 	return Permutations;
 }
